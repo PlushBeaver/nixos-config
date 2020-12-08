8dfb5dc48aaf24daaf0cdc44602bb79590f54b7d
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2020-12-08 17:17:55 +0300

    software: switch from Samba to RDP for QEMU, ditch libvirt

--- software/qemu.nix
+++ software/qemu.nix
@@ -1,29 +1,12 @@
 { pkgs, ... }:
 
-let
-  qemuWithSamba = pkgs.qemu.override {
-    hostCpuOnly = true;
-    smbdSupport = true;
-  };
-in {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemuPackage = qemuWithSamba;
-  };
-
-  users.users."dmitry".extraGroups = [
-    "libvirtd"
-  ];
-
+{
   environment.systemPackages = with pkgs; [
     OVMF
     kvm
-    #(libguestfs.override { qemu = qemuWithSamba; }) -- broken in 20.09
-    libosinfo
-    libvirt
     qemu-utils
-    qemuWithSamba
-    virt-manager
-    virt-viewer
+    (qemu.override { hostCpuOnly = true; })
+    freerdp
+    spice
   ];
 }
