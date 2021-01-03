42132866f3e10843c548b3d24af487c00890e645
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-01-04 02:33:36 +0300

    home-manager: firefox as default browser

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -7,6 +7,11 @@
     xdg.mimeApps = {
       enable = true;
 
+      defaultApplications = {
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+      };
+
       associations.removed =
         let
           noCalibre =
