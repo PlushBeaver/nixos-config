660be0cdd6559b0ead1bc3f1224efe36cf2f3e94
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-03-20 13:34:35 +0300

    docker: fix lingering socat processes

--- software/docker.nix
+++ software/docker.nix
@@ -32,7 +32,7 @@
   ];
 
   systemd.services."docker-dns-proxy" = {
-    script = "${pkgs.socat}/bin/socat UDP-LISTEN:53,fork,reuseaddr,bind=172.17.0.1 UDP:127.0.0.53:53";
+    script = "${pkgs.socat}/bin/socat -T5 UDP-LISTEN:53,fork,reuseaddr,bind=172.17.0.1 UDP:127.0.0.53:53";
     wantedBy = ["docker.service"];
     after = ["docker.service"];
   };
