8f173acbd4b35132b2fdafbad6d3a242ca045832
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-04-18 23:00:01 +0300

    nix: pin nixpkgs to system revision

--- nixos/default.nix
+++ nixos/default.nix
@@ -9,6 +9,9 @@
     autoOptimiseStore = true;
     extraOptions = "experimental-features = nix-command flakes";
     package = pkgs.nixFlakes;
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs;
+    };
     trustedUsers = ["root" "@wheel"];
     gc = {
       automatic = true;
