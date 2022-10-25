8188b502edb434be0f208d203e687093e617dbc1
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2022-10-25 23:22:40 +0300

    vscode: update to 1.68.1 and refresh extension set

--- software/vscode.nix
+++ software/vscode.nix
@@ -1,70 +1,44 @@
-{ pkgs, ... }:
+{ inputs, ... }:
 
 {
-  environment.systemPackages = with pkgs;
-    [
-      (vscode-with-extensions.override {
+  environment.systemPackages =
+    with inputs.nixpkgs-2205.legacyPackages.x86_64-linux;
+    let
+      vscode = vscode-with-extensions.override {
         vscodeExtensions = let
-          packaged = with vscode-extensions; [
-            ms-azuretools.vscode-docker
-            ms-python.python
-            ms-vscode-remote.remote-ssh
-            ms-vscode.cpptools
-            vscodevim.vim
-            golang.go
-          ];
-
-          local = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-            {
-              name = "meson";
-              publisher = "asabil";
-              version = "1.3.0";
-              sha256 = "sha256-QMp3dEFx6Mu5pgzklylW6b/ugYbtbT/qz8IeeuzPZeA=";
-            }
-
-            {
-              publisher = "ms-vscode";
-              name = "cmake-tools";
-              version = "1.6.0";
-              sha256 = "sha256-vI36GI1J21T5Lg5YBm8zfyvgi0kYDwG2wCmXRT83a8g=";
-            }
-
-            {
-              name = "gitlens";
-              publisher = "eamodio";
-              version = "10.2.1";
-              sha256 = "1bh6ws20yi757b4im5aa6zcjmsgdqxvr1rg86kfa638cd5ad1f97";
-            }
-
-            {
-              name = "nix-ide";
-              publisher = "jnoortheen";
-              version = "0.1.7";
-              sha256 = "sha256-NXMwd1Yi6kVxXLhWgzFOXQm580Mv4UPh6jUulbDnhK8=";
-            }
-
-            {
-              name = "nix-env-selector";
-              publisher = "arrterian";
-              version = "0.1.2";
-              sha256 = "sha256-aTNxr1saUaN9I82UYCDsQvH9UBWjue/BSnUmMQOnsdg=";
-            }
-
-            {
-              name = "go-group-imports";
-              publisher = "aleksandra";
-              version = "1.1.3";
-              sha256 = "sha256-G8Z2Twe6dFe2fE2KUOb2FmzW0iagRsjxERQZI0vJqSs=";
-            }
-
-            {
-              name = "restructuredtext";
-              publisher = "lextudio";
-              version = "143.0.0";
-              sha256 = "sha256-RUxdbSn3zhesFyCbaLpd9fHM9bLqOrVX1sPh2cabPmM=";
-            }
-          ];
-        in packaged ++ local;
-      })
+          mk = vscode-utils.extensionFromVscodeMarketplace;
+          mesonbuild.mesonbuild = mk {
+            publisher = "mesonbuild";
+            name = "mesonbuild";
+            version = "1.7.0";
+            sha256 = "sha256-pHHPHF+y5tu0IG6hbdM1LJLlDia0cnRLLWDBBP81TnU=";
+          };
+          trond-snekvik.simple-rst = mk {
+            publisher = "trond-snekvik";
+            name = "simple-rst";
+            version = "1.5.2";
+            sha256 = "sha256-pV7/S8kkDIbhD2K5P2TA8E0pM4F8gsFIlc+4FIheBbc=";
+          };
+        in with vscode-extensions; [
+          arrterian.nix-env-selector
+          eamodio.gitlens
+          editorconfig.editorconfig
+          golang.go
+          llvm-vs-code-extensions.vscode-clangd
+          mesonbuild.mesonbuild
+          ms-azuretools.vscode-docker
+          ms-python.python
+          ms-python.vscode-pylance
+          ms-vscode-remote.remote-ssh
+          timonwong.shellcheck
+          trond-snekvik.simple-rst
+          vscodevim.vim
+        ];
+      };
+    in [
+      clang_14 # C/C++ LS
+      docutils # reStructuredText LS
+      gopls # Go LS
+      vscode
     ];
 }
