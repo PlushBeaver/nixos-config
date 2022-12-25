80af3a34f3f2288cdf570187b8eeb13a548aaf7f
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2022-12-25 13:41:18 +0300

    vscode: go template plugin

--- software/vscode.nix
+++ software/vscode.nix
@@ -19,11 +19,18 @@
             version = "1.5.2";
             sha256 = "sha256-pV7/S8kkDIbhD2K5P2TA8E0pM4F8gsFIlc+4FIheBbc=";
           };
+          jinliming2.vscode-go-template = mk {
+            publisher = "jinliming2";
+            name = "vscode-go-template";
+            version = "0.2.1";
+            sha256 = "sha256-kvWaSuE4O98fXU+EKkplFI0Cfy2I7IiJ0/hXrVsk+6g=";
+          };
         in with vscode-extensions; [
           arrterian.nix-env-selector
           eamodio.gitlens
           editorconfig.editorconfig
           golang.go
+          jinliming2.vscode-go-template
           llvm-vs-code-extensions.vscode-clangd
           mesonbuild.mesonbuild
           ms-azuretools.vscode-docker
